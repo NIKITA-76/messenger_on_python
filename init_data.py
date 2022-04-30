79c4f3570933f547578046b26f924e46e329b248
Tarasov Nikita <89522179992.ru@gmail.com>
2022-04-30 14:25:35 +0300

    Data initialization (connecting to the database, creating lists and dictionaries)

---
+++ init_data.py
@@ -0,0 +1,19 @@
+from configparser import ConfigParser
+from pymongo import MongoClient
+
+
+class Data:
+    def __init__(self):
+        self.configfile = "config.ini"
+        self.config = ConfigParser()
+        self.config.read(self.configfile)
+        self.client = MongoClient(self.config["database"]["ip"], int(self.config["database"]["port"]))
+        self.collect = self.client["Messanger"]
+        self.DB = self.collect["msgr"]
+        self.userAndObject = {}
+        self.rooms = {}
+        self.users = []  # List of all users
+        self.users_ip = []  # List of all users-ip
+        self.name_withIp = {}
+        self.idUser = self.DB.find_one({"_id": "COUNT"})["USERS"]
+        self.idRoom = self.DB.find_one({"_id": "COUNT"})["ROOMS"]
