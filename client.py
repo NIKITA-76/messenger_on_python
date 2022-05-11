1618ad0ff2a6a9245467f8a3db2a25936b1ee81c
Tarasov Nikita <89522179992.ru@gmail.com>
2022-05-11 16:39:44 +0300

    Sending files(only small files)

--- client.py
+++ client.py
@@ -6,7 +6,8 @@ import threading
 import ui_client
 
 from configparser import ConfigParser
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QFileDialog
 
 
 class Client(socket.socket):
@@ -21,8 +22,9 @@ class Client(socket.socket):
         self.send(data_text)
 
 
-class Ui_MainWindow(ui_client.UI_ForMain):
+class Ui_MainWindow(ui_client.UI_ForMain, QWidget):
     def __init__(self, MainWindow, ChildWindow):
+        super().__init__()
         super().setupUi(MainWindow, ChildWindow, )
         self.cl = Client()
         self.thr = threading.Thread(target=self.get_text).start()
@@ -50,20 +52,17 @@ class Ui_MainWindow(ui_client.UI_ForMain):
                 self.pushButton_menu.show()
                 self.pushButton_settings.show()
                 self.load()
-
             elif data[0] == "USER_IS_NOT_SIGN":
                 self.LP_RForm.label.setText("Неправлиьный логин или пароль")
                 self.LP_RForm.lineEdit.setStyleSheet("border: 1px solid rgb(94, 0, 1);")
                 self.LP_RForm.lineEdit_2.setStyleSheet("border: 1px solid rgb(94, 0, 1);")
                 print("USER_IS_NOT_SIGN")
-
             elif data[0] == "MSGROOM":
                 if self.listWidget_people.currentItem().text() == data[3]:
                     self.listWidget_msgRoom.addItem(f"{data[1]}: {' '.join(data[2])}")
                     time.sleep(0.01)
                     self.listWidget_msgRoom.verticalScrollBar().setValue(
                         self.listWidget_msgRoom.verticalScrollBar().maximum())
-
             elif data[0] == "CRT_ROOM":
                 try:
                     self.roomsForLoad[0].update(data[1])
@@ -71,12 +70,10 @@ class Ui_MainWindow(ui_client.UI_ForMain):
                     self.roomsForLoad.append(data[1])
                 for _, value in data[1].items():
                     self.listWidget_people.addItem(value)
-
             elif data[0] == "LOADMSG_ADD":
                 data[1].reverse()
                 self.listWidget_msgRoom.verticalScrollBar().setValue(3)
                 self.listWidget_msgRoom.insertItems(0, data[1])
-
             elif data[0] == "LOADMSG":
                 if data[1] == "NOMSG":
                     self.listWidget_msgRoom.clear()
@@ -90,7 +87,6 @@ class Ui_MainWindow(ui_client.UI_ForMain):
                     time.sleep(0.01)
                     self.listWidget_msgRoom.verticalScrollBar().setValue(
                         self.listWidget_msgRoom.verticalScrollBar().maximum())
-
             elif data[0] == "SEARCH":
                 match_of_people = pickle.loads(data[1])
                 self.AddFRNDForm.listWidget.clear()
@@ -195,6 +191,14 @@ class Ui_MainWindow(ui_client.UI_ForMain):
         self.AddFRNDWindow.close()
         self.cl.send_data(new_room)
 
+    def get_path_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, )
+        file = open(str(file_path), mode="rb")
+        print(_)
+        data = ["FILE", self.nick_name, self.listWidget_people.currentItem().text(), file.read(2048)]
+        data = pickle.dumps(data)
+        self.cl.send_data(data)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
@@ -204,7 +208,5 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow, ChildWindow)
     MainWindow.show()
     ChildWindow.show()
-    ChildWindow.setWindowState(QtCore.Qt.WindowActive)
-    print(ChildWindow.windowState())
     app.lastWindowClosed.connect(ui.shotdown)
     sys.exit(app.exec_())
